f11a863e2b8580fe4a1d2e5e3eea872987e5dcba
Pauliina Solanne <pauliinasolanne@Pauliina-Air.zyxel.com>
2016-06-10 16:15:51 +0300

    second version

--- js/app.js
+++ js/app.js
@@ -1,31 +1,53 @@
 console.log('I am ready');
 
 $(document).ready(function() {
-    $('#button').mouseenter(function() {
-        $('#button').fadeTo('fast', 0.5);
+    $('.button').mouseenter(function() {
+        $(this).fadeTo('fast', 0.5);
     });
-    $('#button').mouseleave(function() {
-        $('#button').fadeTo('fast', 1.0);
+    $('.button').mouseleave(function() {
+        $(this).fadeTo('fast', 1.0);
     });
 });
 
 
-    $("#button").click(function() {
-        console.log("Btn Clicked");
+// $(".button").click(function() {
+//     console.log("Btn Clicked");
+//
+// });
 
-      });
+$(document).ready(function() {
+    $("#add-new").click(function() {
+        console.log("Adding");
 
-      jQuery(document).on('ready', function() {
-          jQuery('form#search-form').bind('submit', function(event){
-              event.preventDefault();
+          var newInput = $("<p>Additional info</p><input name='additional-info' type='text' class='searchbox' placeholder='Additional info'>");
+          $('input#product-currency').after(newInput);
+        });
 
-              var tbody = jQuery('#product-list > tbody');
+    });
 
-              tbody.append('<tr><th scope="row" style="background-color:' + this['new-task-color'].value +
-                  '"><input type="checkbox" /></th><td>' + this['new-task-date'].value +
-                  '</td><td>' + this['new-task-priority'].value + '</td><td>' + this['new-task-name'].value +
-                  '</td><td>' + this['new-task-desc'].value + '</td><td>' + this['new-task-email'].value + '</td></tr>');
 
-              return false;
-          });
-      });
+// Adds new features to the product
+
+$(document).on('ready', function() {
+    $('form#search-form').bind('submit', function(event) {
+        event.preventDefault();
+
+        var tbody = $('#product-list > tbody');
+
+        tbody.append('<tr><th scope="row" style="background-color:' + this['new-task-color'].value +
+            '"><input type="checkbox" /></th><td>' + this['product-name'].value +
+            '</td><td>' + this['product-price'].value + '</td><td>' + this['product-link'].value +
+            '</td><td>' + this['product-currency'].value + '</td><td>');
+
+        return false;
+    });
+});
+
+// Creates a table for the form objects
+
+$("form").submit(function(event) {
+    console.log($(this).serializeArray());
+    event.preventDefault();
+});
+
+// Shows the JSON in the console
