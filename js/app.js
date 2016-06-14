f3bafa9d57a033e8155ac49fa2d2f5897bcd4a12
Pauliina Solanne <pauliinasolanne@Pauliina-Air.zyxel.com>
2016-06-14 15:16:30 +0300

    changes

--- js/app.js
+++ js/app.js
@@ -12,13 +12,13 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#add-new").click(function() {
 
-          newInput = $("<div class='newfeature'><p>Additional info <button id='remove'>Remove feature</button> </p> <input name='additional-info' id='additional-info' type='text' class='searchbox' placeholder='Additional info'></div>");
-          $('input#product-currency').after(newInput);
-          $("#remove").click(function() {
+        newInput = $("<div class='newfeature'><p>Additional info <button id='remove'>Remove feature</button> </p> <input name='additional-info' id='additional-info' type='text' class='searchbox' placeholder='Additional info'></div>");
+        $('input#product-currency').after(newInput);
+        $("#remove").click(function() {
             $(newInput).remove();
-          });
         });
     });
+});
 
 // Adds a new dynamic feature to the form
 
@@ -32,15 +32,16 @@ $("form").submit(function(event) {
 // Shows the JSON in the console
 
 $(document).ready(function() {
-  $("#open-modal").on("click", openModal);
-  $("#close-modal").on("click", closeModal);
+    $("#open-modal").on("click", openModal);
+    $("#close-modal").on("click", closeModal);
 });
 
 function openModal() {
-  $("#modal").toggle();
+    $("#modal").toggle();
 }
+
 function closeModal() {
-  $("#modal").toggle();
+    $("#modal").toggle();
 }
 // Handles the JSON modal
 
@@ -49,34 +50,34 @@ $(document).on('ready', function() {
         event.preventDefault();
         var tbody = $('#product-list > tbody');
 
-            if (json.length == 5) {
-              console.log("Additional feature doesn't exist");
-              tbody.append('<tr><th scope="row" style="background-color:' + this['new-product-color'].value +
-                  '"> </th><td>' + this['product-name'].value +
-                  '</td><td>' + this['product-price'].value + '</td><td>' + this['product-link'].value +
-                  '</td><td>' + this['product-currency'].value + '</td><td>' + ' ');
-            } else if (json[5].value){
-              console.log("Additional feature exists");
-              tbody.append('<tr><th scope="row" style="background-color:' + this['new-product-color'].value +
-                  '"> </th><td>' + this['product-name'].value +
-                  '</td><td>' + this['product-price'].value + '</td><td>' + this['product-link'].value +
-                  '</td><td>' + this['product-currency'].value + '</td><td>' + this['additional-info'].value);
-            }
+        if (json.length == 5) {
+            console.log("Additional feature doesn't exist");
+            tbody.append('<tr><th scope="row" style="background-color:' + this['new-product-color'].value +
+                '"> </th><td>' + this['product-name'].value +
+                '</td><td>' + this['product-price'].value + '</td><td>' + this['product-link'].value +
+                '</td><td>' + this['product-currency'].value + '</td><td>' + ' ');
+        } else if (json[5].value) {
+            console.log("Additional feature exists");
+            tbody.append('<tr><th scope="row" style="background-color:' + this['new-product-color'].value +
+                '"> </th><td>' + this['product-name'].value +
+                '</td><td>' + this['product-price'].value + '</td><td>' + this['product-link'].value +
+                '</td><td>' + this['product-currency'].value + '</td><td>' + this['additional-info'].value);
+        }
     });
 });
 
 // Submits the form information to the table
 
 $("form").submit(function(event) {
-     var formData = $(this).serializeArray();
-     var jsonData = JSON.stringify(formData);
-     console.log(jsonData);
-     var newHTML = [];
-     for (var i = 0; i < jsonData.length; i++) {
-         newHTML.push('<span>' + jsonData[i] + '</span>');
-     }
-     $(".modal-body").html('<p>JSON:<p>' + newHTML.join(""));
-     event.preventDefault();
-   });
+    var formData = $(this).serializeArray();
+    var jsonData = JSON.stringify(formData);
+    console.log(jsonData);
+    var newHTML = [];
+    for (var i = 0; i < jsonData.length; i++) {
+        newHTML.push('<span>' + jsonData[i] + '</span>');
+    }
+    $(".modal-body").html('<p>JSON:<p>' + newHTML.join(""));
+    event.preventDefault();
+});
 
 // Shows the json data of new products
