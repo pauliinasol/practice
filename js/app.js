4723734e319585fb10ec0e0c21f08a9e6431dc44
Pauliina Solanne <pauliinasolanne@Pauliina-Air.zyxel.com>
2016-06-14 14:44:57 +0300

    muutokset

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-console.log('I am ready');
+
+
 
 $(document).ready(function() {
     $('.button').mouseenter(function() {
@@ -10,44 +11,83 @@ $(document).ready(function() {
 });
 
 
-// $(".button").click(function() {
-//     console.log("Btn Clicked");
-//
-// });
 
 $(document).ready(function() {
     $("#add-new").click(function() {
-        console.log("Adding");
 
-          var newInput = $("<p>Additional info</p><input name='additional-info' type='text' class='searchbox' placeholder='Additional info'>");
+          newInput = $("<div class='newfeature'><p>Additional info <button id='remove'>Remove feature</button> </p> <input name='additional-info' id='additional-info' type='text' class='searchbox' placeholder='Additional info'></div>");
           $('input#product-currency').after(newInput);
+          $('.adding').toggle();
+
+          $("#remove").click(function() {
+            $(newInput).remove();
+          });
         });
-
     });
 
+// Adds a new dynamic feature to the form
 
-// Adds new features to the product
-
-$(document).on('ready', function() {
-    $('form#search-form').bind('submit', function(event) {
-        event.preventDefault();
-
-        var tbody = $('#product-list > tbody');
-
-        tbody.append('<tr><th scope="row" style="background-color:' + this['new-task-color'].value +
-            '"><input type="checkbox" /></th><td>' + this['product-name'].value +
-            '</td><td>' + this['product-price'].value + '</td><td>' + this['product-link'].value +
-            '</td><td>' + this['product-currency'].value + '</td><td>');
-
-        return false;
-    });
-});
-
-// Creates a table for the form objects
 
 $("form").submit(function(event) {
-    console.log($(this).serializeArray());
+    // console.log($(this).serializeArray());
+    json = $(this).serializeArray();
+
+    console.log(json);
     event.preventDefault();
 });
 
 // Shows the JSON in the console
+
+$(document).ready(function() {
+  $("#open-modal").on("click", openModal);
+  $("#close-modal").on("click", closeModal);
+});
+
+function openModal() {
+  $("#modal").toggle();
+}
+
+function closeModal() {
+  $("#modal").toggle();
+}
+
+// Handles the JSON modal
+
+$(document).on('ready', function() {
+    $('form#search-form').bind('submit', function(event) {
+        event.preventDefault();
+        var tbody = $('#product-list > tbody');
+
+
+            if (json.length == 5) {
+              console.log("happydays");
+              tbody.append('<tr><th scope="row" style="background-color:' + this['new-product-color'].value +
+                  '"><input type="checkbox" class="checkbox" /></th><td>' + this['product-name'].value +
+                  '</td><td>' + this['product-price'].value + '</td><td>' + this['product-link'].value +
+                  '</td><td>' + this['product-currency'].value + '</td><td>' + ' ');
+            } else if (json[5].value){
+              console.log("false");
+              tbody.append('<tr><th scope="row" style="background-color:' + this['new-product-color'].value +
+                  '"><input type="checkbox" class="checkbox" /></th><td>' + this['product-name'].value +
+                  '</td><td>' + this['product-price'].value + '</td><td>' + this['product-link'].value +
+                  '</td><td>' + this['product-currency'].value + '</td><td>' + this['additional-info'].value);
+            }
+
+    });
+});
+
+// Submits the form information to the table
+
+$("form").submit(function(event) {
+     var formData = $(this).serializeArray();
+     var jsonData = JSON.stringify(formData);
+     console.log(jsonData);
+     var newHTML = [];
+     for (var i = 0; i < jsonData.length; i++) {
+         newHTML.push('<span>' + jsonData[i] + '</span>');
+     }
+     $(".modal-body").html('<p>JSON:<p>' + newHTML.join(""));
+     event.preventDefault();
+   });
+
+// Shows the json data of new products
